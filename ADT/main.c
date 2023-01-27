3ca342c000819cd1fb358a38fa3ef1dd3b26147f
shayrin0 <shayrin.panahi@gmail.com>
2023-01-26 22:05:13 -0500

    add BinearySearch function

--- ADT/main.c
+++ ADT/main.c
@@ -77,6 +77,27 @@ int LinearSearch(struct Array *arr, int key)
     return -1;
 }
 
+// Bineary search to find an element
+int BinarySearch(struct Array arr, int key)
+{
+    int l, mid, h;
+    l=0;
+    h=arr.length-1;
+    
+    while(l <= h)
+    {
+        mid = (l+h)/2;
+        if (key == arr.A[mid])
+            return mid;
+        else if (key < arr.A[mid])
+            h = mid-1;
+        else
+            l = mid+1;
+    }
+    
+    return -1;
+}
+
 int main()
 {
     struct Array arr={{2,3,4,5,6}, 10,5};
@@ -84,7 +105,8 @@ int main()
     // Append(&arr, 10);
     // Insert(&arr, 3, 10);
     // printf("%d\n", Delete(&arr, 1));
-    printf("%d\n", LinearSearch(&arr, 4));
+    // printf("%d\n", LinearSearch(&arr, 4));
+    printf("%d\n", BinarySearch(arr, 4));
     Display(arr);
 
     return 0;
