8611ef0c1b6c28b6d291568c7babbbbfa88590e8
shayrin0 <shayrin.panahi@gmail.com>
2023-02-14 19:18:28 -0500

    initial commit

---
+++ Examples/bitwise_op.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+    Test the bitwise operators
+*/
+
+int main()
+{
+    int32_t num1, num2;
+
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    return 0;
+}
