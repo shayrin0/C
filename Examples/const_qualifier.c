ce9444e1ca4180566f713dea759a86f98465252f
shayrin0 <shayrin.panahi@gmail.com>
2023-02-14 21:18:40 -0500

    change value of a const variable

--- Examples/const_qualifier.c
+++ Examples/const_qualifier.c
@@ -1,6 +1,25 @@
 
 /*
     Although we can't change the value of a const variable using its name,
-    its value can still be changed using the variable's address
+    its value can still be changed using the variable's address.
+
+    'const' doesn't mean that the value never changes, it is only a programming
+    safety feature to ensure that the programmer wouldn't try to modify the value
 */
 
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+    uint8_t const data = 10;
+
+    printf("Value = %u.\n", data);
+
+    uint8_t *ptr = (uint8_t*) &data;
+    *ptr = 50;
+
+    printf("Value = %u.\n", data);
+
+    return 0;
+}
