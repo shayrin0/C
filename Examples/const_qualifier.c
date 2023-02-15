d2d1e904370c4e7e437da9026ca2bdbce00fb68d
shayrin0 <shayrin.panahi@gmail.com>
2023-02-14 21:24:14 -0500

    comment on placement of const variable in memory

--- Examples/const_qualifier.c
+++ Examples/const_qualifier.c
@@ -5,6 +5,12 @@
 
     'const' doesn't mean that the value never changes, it is only a programming
     safety feature to ensure that the programmer wouldn't try to modify the value
+
+    - All local const variables are placed in RAM.
+    - All global const variables are placed in ROM or FLASH.
+    - In STM32, all global const variables live in FLASH memory. So, when you
+      try to modify const variable using its address, operation has no effect.
+      Because flash memory of the MCU is write-protected.
 */
 
 #include <stdio.h>
