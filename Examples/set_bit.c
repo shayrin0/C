981426012cf4c4d37e6ec9f4b59cf0d083ea9f59
shayrin0 <shayrin.panahi@gmail.com>
2023-02-14 19:33:38 -0500

    initial commit

---
+++ Examples/set_bit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+    Write a program to set the 4th and 7th bit positions
+    of a given number.
+    For set operation, the bit-wise OR should be used.
+*/
+
+int main()
+{
+    int32_t num;
+
+    return 0;
+}
