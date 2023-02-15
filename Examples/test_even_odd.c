102964b20640eb3d61d160afe2ee96dd386bd1e8
shayrin0 <shayrin.panahi@gmail.com>
2023-02-14 19:27:07 -0500

    initial commit

---
+++ Examples/test_even_odd.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+    Check if a number is even or odd using Bit-Masking
+    even number: the lsb is zero
+    odd number: the lsb is one
+    for checking the lsb, the mask should have 1 in its lsb
+*/
