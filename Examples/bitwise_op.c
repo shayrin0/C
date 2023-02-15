2179fd85f89ec06afb59b88197e9ea3d56ee60c2
shayrin0 <shayrin.panahi@gmail.com>
2023-02-14 19:23:13 -0500

    implement bitwise operators

--- Examples/bitwise_op.c
+++ Examples/bitwise_op.c
@@ -12,5 +12,10 @@ int main()
     printf("Enter 2 numbers: ");
     scanf("%d %d", &num1, &num2);
 
+    printf("Bitwise AND(&) : %d\n", (num1 & num2)); // testing bits
+    printf("Bitwise OR(|) : %d\n", (num1 | num2)); // setting bits
+    printf("Bitwise XOR(^) : %d\n", (num1 ^ num2)); // clearing bits
+    printf("Bitwise NOT(~) : ~num1=%d, ~num2=%d\n", num1, num2); // toggling bits
+
     return 0;
 }
