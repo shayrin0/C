3392a918c62c843735abe8ec8f5bf628e4cefcf0
shayrin0 <shayrin.panahi@gmail.com>
2023-02-14 20:05:21 -0500

    initial commit

---
+++ STM32/led_ex.c
@@ -0,0 +1,10 @@
+
+
+/*
+    Turn on the LED:
+    1. Identify the GPIO port used to connect the LED
+    2. Identify the GPIO pin where the LED is connected
+    3. Activate the GPIO peripheral (Enable the clock)
+    4. Configure the GPIO pin mode as output
+    5. Write to the GPIO pin
+*/
