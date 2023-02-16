4d410c1883bf0b1e5da4269ddd419017e4d14db6
shayrin0 <shayrin.panahi@gmail.com>
2023-02-15 20:28:56 -0500

    mask different parts

--- Examples/extract_info.c
+++ Examples/extract_info.c
@@ -25,10 +25,28 @@ int main()
 {
     uint32_t packetValue;
     printf("Enter the 32bit packet value:");
-    scanf("%u", &packetValue);
+    scanf("%X", &packetValue);
 
     struct Packet packet;
-    
+
+    // crc is the first 2 bits
+    packet.crc = (uint8_t) (packetValue & 0x3);
+    packet.status = (uint8_t) ((packetValue >> 2) & 0x1);
+    packet.payload = (uint16_t) ((packetValue >> 3) & 0xFFF);
+    packet.bat = (uint8_t) ((packetValue >> 15) & 0x7);
+    packet.sensor = (uint8_t) ((packetValue >> 18) & 0x7);
+    packet.longAddr = (uint8_t) ((packetValue >> 21) & 0xFF);
+    packet.shortAddr = (uint8_t) ((packetValue >> 29) & 0x3);
+    packet.addrMode = (uint8_t) ((packetValue >> 31) & 0x3);
+
+    printf("crc        :%#x\n", packet.crc);
+    printf("status     :%#x\n", packet.status);
+    printf("payload    :%#x\n", packet.payload);
+    printf("bat        :%#x\n", packet.bat);
+    printf("sensor     :%#x\n", packet.sensor);
+    printf("longAddr   :%#x\n", packet.longAddr);
+    printf("shortAddr  :%#x\n", packet.shortAddr);
+    printf("addrMode   :%#x\n", packet.addrMode);
 
     return 0;
 }
