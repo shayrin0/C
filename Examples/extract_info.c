b4fb98483d0356c1499e599f4ec27ed18dafcff5
shayrin0 <shayrin.panahi@gmail.com>
2023-02-15 20:12:52 -0500

    initial commit

---
+++ Examples/extract_info.c
@@ -0,0 +1,6 @@
+
+/*
+    Write a program to decode a given 32bit packet information and
+    print the values of different fields. Create a structure with
+    member elements as packet fields.
+*/
