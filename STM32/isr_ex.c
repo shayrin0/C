91b287ad006fd8b2b259f411207752b1682be0fd
shayrin0 <shayrin.panahi@gmail.com>
2023-02-15 19:08:16 -0500

    add registers addresses

--- STM32/isr_ex.c
+++ STM32/isr_ex.c
@@ -9,7 +9,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
+uint8_t g_button_pressed = 0;
+
+uint32_t g_button_press_count = 0;
+
+void button_init(void);
+
+uint32_t volatile *pEXTTIPendReg = (uint32_t*)0x40013C14;
+uint32_t volatile *pClkCtrlReg = (uint32_t*)0x40023830;
+uint32_t volatile *pClkCtrlRegApb2 = (uint32_t*)0x40023844;
+uint32_t volatile *pGPIOAModeReg = (uint32_t*)0x40020000;
+uint32_t volatile *pEXTTIMaskReg = (uint32_t*)0x40013C00;
+uint32_t volatile *pEXTTIEdgeCtrlReg = (uint32_t*)0x40013C08;
+uint32_t volatile *pNVICIRQEnReg = (uint32_t*)0xE000E100;
+
 int main()
 {
-    
+
 }
