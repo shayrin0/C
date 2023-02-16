656943e2b04e9a2d201b80ea36fd0fb526ba11d6
shayrin0 <shayrin.panahi@gmail.com>
2023-02-15 20:15:15 -0500

    add struct information

--- Examples/extract_info.c
+++ Examples/extract_info.c
@@ -4,3 +4,25 @@
     print the values of different fields. Create a structure with
     member elements as packet fields.
 */
+
+#include <stdio.h>
+#include <stdint.h>
+
+struct Packet
+{
+    uint8_t crc;
+    uint8_t status;
+    uint16_t payload;
+    uint8_t bat;
+    uint8_t sensor;
+    uint8_t longAddr;
+    uint8_t shortAddr;
+    uint8_t addrMode;
+};
+
+
+int main()
+{
+
+    return 0;
+}
