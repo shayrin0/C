437b4879d03e3e30a26e3eb0115a00e0d2f0614c
shayrin0 <shayrin.panahi@gmail.com>
2023-02-15 20:33:35 -0500

    update code

--- Examples/extract_info.c
+++ Examples/extract_info.c
@@ -10,14 +10,14 @@
 
 struct Packet
 {
-    uint32_t crc:2;
-    uint32_t status:1;
-    uint32_t payload:12;
-    uint32_t bat:3;
-    uint32_t sensor:3;
-    uint32_t longAddr:8;
-    uint32_t shortAddr:2;
-    uint32_t addrMode:1;
+    uint32_t crc        :2;
+    uint32_t status     :1;
+    uint32_t payload    :12;
+    uint32_t bat        :3;
+    uint32_t sensor     :3;
+    uint32_t longAddr   :8;
+    uint32_t shortAddr  :2;
+    uint32_t addrMode   :1;
 };
 
 
