781b09fc85f611746213fde452c1a17ccb56596a
shayrin0 <shayrin.panahi@gmail.com>
2023-02-16 21:33:47 -0500

    use union to save more memory

--- Examples/extract_info.c
+++ Examples/extract_info.c
@@ -8,45 +8,41 @@
 #include <stdio.h>
 #include <stdint.h>
 
-struct Packet
+union Packet
 {
-    uint32_t crc        :2;
-    uint32_t status     :1;
-    uint32_t payload    :12;
-    uint32_t bat        :3;
-    uint32_t sensor     :3;
-    uint32_t longAddr   :8;
-    uint32_t shortAddr  :2;
-    uint32_t addrMode   :1;
+    uint32_t packetValue;
+
+    struct 
+    {
+        uint32_t crc        :2;
+        uint32_t status     :1;
+        uint32_t payload    :12;
+        uint32_t bat        :3;
+        uint32_t sensor     :3;
+        uint32_t longAddr   :8;
+        uint32_t shortAddr  :2;
+        uint32_t addrMode   :1;
+    }packetFields;
 };
 
 
 int main()
 {
-    uint32_t packetValue;
+    union Packet packet;
+
     printf("Enter the 32bit packet value:");
-    scanf("%X", &packetValue);
+    scanf("%X", &packet.packetValue);
 
-    struct Packet packet;
+    printf("crc        :%#x\n", packet.packetFields.crc);
+    printf("status     :%#x\n", packet.packetFields.status);
+    printf("payload    :%#x\n", packet.packetFields.payload);
+    printf("bat        :%#x\n", packet.packetFields.bat);
+    printf("sensor     :%#x\n", packet.packetFields.sensor);
+    printf("longAddr   :%#x\n", packet.packetFields.longAddr);
+    printf("shortAddr  :%#x\n", packet.packetFields.shortAddr);
+    printf("addrMode   :%#x\n", packet.packetFields.addrMode);
 
-    // crc is the first 2 bits
-    packet.crc = (uint8_t) (packetValue & 0x3);
-    packet.status = (uint8_t) ((packetValue >> 2) & 0x1);
-    packet.payload = (uint16_t) ((packetValue >> 3) & 0xFFF);
-    packet.bat = (uint8_t) ((packetValue >> 15) & 0x7);
-    packet.sensor = (uint8_t) ((packetValue >> 18) & 0x7);
-    packet.longAddr = (uint8_t) ((packetValue >> 21) & 0xFF);
-    packet.shortAddr = (uint8_t) ((packetValue >> 29) & 0x3);
-    packet.addrMode = (uint8_t) ((packetValue >> 31) & 0x3);
-
-    printf("crc        :%#x\n", packet.crc);
-    printf("status     :%#x\n", packet.status);
-    printf("payload    :%#x\n", packet.payload);
-    printf("bat        :%#x\n", packet.bat);
-    printf("sensor     :%#x\n", packet.sensor);
-    printf("longAddr   :%#x\n", packet.longAddr);
-    printf("shortAddr  :%#x\n", packet.shortAddr);
-    printf("addrMode   :%#x\n", packet.addrMode);
+    printf("Size of union is %I64lu.\n", sizeof(packet));
 
     return 0;
 }
