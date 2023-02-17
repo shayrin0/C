c19b3f2de83873ec31640a1bbfdc6e4c460efe1b
shayrin0 <shayrin.panahi@gmail.com>
2023-02-16 23:01:04 -0500

    enable pull-up registors for PD 8-11

--- STM32/Keypad/keypad_ex.c
+++ STM32/Keypad/keypad_ex.c
@@ -16,7 +16,7 @@ int main()
     uint32_t volatile *const pClockCtrlReg   =   (uint32_t*) (0x40023800+0x30);
     uint32_t volatile *const pPullupDownReg  =   (uint32_t*) (0x40020C00+0x0C);
 
-    // 1. Enable the peripheral clock of GPIOD peripheral
+    // 1. enable the peripheral clock of GPIOD peripheral
     *pClockCtrlReg |= (1 << 3);
 
     // 2. configure PD0, PD1, PD2, PD3 as output (rows of the keypad)
@@ -25,4 +25,8 @@ int main()
 
     // 3. configure PD8, PD9, PD10, PD11 as input (columns of the keypad)
     *pGPIODModeReg &= ~(0xFF << 16); // clear the bits to make the pins as input
+
+    // 4. enable internal pull-up registors for PD8, PD9, PD10, PD11
+    *pGPIODModeReg &= ~(0xFF << 16); // clear
+    *pGPIODModeReg |= (0x55 << 16);
 }
