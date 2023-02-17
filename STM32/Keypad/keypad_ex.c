7a3d2d4d6a8f87ffac4b0a3396ad4b57f437fab5
shayrin0 <shayrin.panahi@gmail.com>
2023-02-16 22:54:13 -0500

    enable the clock for gpio d

--- STM32/Keypad/keypad_ex.c
+++ STM32/Keypad/keypad_ex.c
@@ -9,4 +9,13 @@
 
 int main()
 {
+    // peripheral register addresses
+    uint32_t volatile *const pGPIODModeReg   =   (uint32_t*) (0x40020C00);
+    uint32_t volatile *const pInPutDataReg   =   (uint32_t*) (0x40020C00+0x10);
+    uint32_t volatile *const pOutPutDataReg  =   (uint32_t*) (0x40020C00+0x14);
+    uint32_t volatile *const pClockCtrlReg   =   (uint32_t*) (0x40023800+0x30);
+    uint32_t volatile *const pPullupDownReg  =   (uint32_t*) (0x40020C00+0x0C);
+
+    // 1. Enable the peripheral clock of GPIOD peripheral
+    *pClockCtrlReg |= (1 << 3);
 }
